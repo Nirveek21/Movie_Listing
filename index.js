0d2859fac922b519f70a8648cae3a5366af12bed
Nirveek21 <nirveek21@gmail.com>
2021-10-11 16:52:51 +0530

    Resolved image urls

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
         $.getJSON("http://demo.tech2edge.co/samples/data-sg", function (data){
           $("#image").append(`<img
-        id="myImg"
-        /*src="${data.series.img}"*/
-        src="https://i.ytimg.com/vi/gS8hQqkVX2g/maxresdefault.jpg"
+        id="myImg"        
+        src="http://demo.tech2edge.co/samples/${(data.series.img)}"
         alt="sacred games"
         style="width: 100%; height: 300px"
       />`)
@@ -14,12 +13,13 @@ $(document).ready(function () {
           console.log(data.characters);
           for(i in data.characters){
               console.log(data.characters[i].age);
+              console.log(data.characters[i].img);
           $(".characters").append( `<div class="shadow-lg p-3 mb-4 bg-body rounded">    <div class="row">
         <div class="col-3" ><a href="#openModal" class="openlink"> 
-          <div class="service">   
-                <img src="${data.characters[i].img}" height="150" width="200"> 
+          <div class="service col-12 col-md-8">   
+                <img src="http://demo.tech2edge.co/samples/${(data.characters[i].img)}" height="150" width="200"> 
           </div></a></div>
-          <div class="col-9">
+          <div class="col-12 col-md-8">
         <div>NAME - ${(data.characters[i].name)} </div>
         <div>AGE - ${(data.characters[i].age)} </div> 
         <div>PROFESSION- ${(data.characters[i].profession)} </div>
